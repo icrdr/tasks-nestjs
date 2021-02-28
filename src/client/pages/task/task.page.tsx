928aec164df784dca8b72575cab888dfbdd18036
manan <icrdr2010@outlook.com>
2021-03-01 00:15:47 +0800

    1

--- src/client/pages/task/task.page.tsx
+++ src/client/pages/task/task.page.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
-import { PageContainer } from '@ant-design/pro-layout';
-import { useRequest } from 'umi';
-import { Select, Space } from 'antd';
-import TaskTable from './components/TaskTable';
-import TaskGallery from './components/TaskGallery';
+import React, { useState } from "react";
+import { PageContainer } from "@ant-design/pro-layout";
+import { useRequest } from "umi";
+import { Select, Space } from "antd";
+import TaskTable from "./components/TaskTable";
+import TaskGallery from "./components/TaskGallery";
 
 const Task: React.FC<{}> = () => {
-  const [viewType, setViewType] = useState('gallery');
+  const [viewType, setViewType] = useState(
+    localStorage.getItem("viewType") || "table"
+  );
+
   return (
     <PageContainer content="管理所有任务">
-      <Space size="middle" direction="vertical" style={{ width: '100%' }}>
-        <Select value={viewType} onChange={(v) => setViewType(v)}>
+      <Space size="middle" direction="vertical" style={{ width: "100%" }}>
+        <Select
+          value={viewType}
+          onChange={(v) => {
+            setViewType(v);
+            localStorage.setItem("viewType", v);
+          }}
+        >
           <Select.Option value="table">表格</Select.Option>
           <Select.Option value="gallery">画廊</Select.Option>
         </Select>
-        {viewType === 'table' && <TaskTable />}
-        {viewType === 'gallery' && <TaskGallery />}
+        {viewType === "table" && <TaskTable />}
+        {viewType === "gallery" && <TaskGallery />}
       </Space>
     </PageContainer>
   );
